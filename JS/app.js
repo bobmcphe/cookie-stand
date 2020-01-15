9058170d3607ec644e53c39d4ef255c761478855
bobby mcpherson <bobmcphe@gmail.com>
2020-01-15 15:42:57 -0600

    added two new stores

--- JS/app.js
+++ JS/app.js
@@ -76,8 +76,175 @@ render: function(){
 }
 }
 
+var tokyo = {
+
+  name: 'tokyo',
+  maxCustomers: 24,
+  minCustomers: 3,
+  avgCookiesPerC: 1.2, 
+  aveCookieSalesPerHr: [],
+  
+
+
+  getRandom: function() {
+    var final = (Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
+    return Math.floor(final);
+  },
+
+
+  getHourlySales: function() {
+        var totalCookiesPerHour = 0;
+
+      for (var i=0; i<hours.length; i++) {
+        var customersPerHour = this.getRandom();
+        console.log(customersPerHour)
+        var totalCookiesPerHour = Math.floor((customersPerHour * this.avgCookiesPerC));
+        this.aveCookieSalesPerHr.push(totalCookiesPerHour);
+
+        console.log(this.aveCookieSalesPerHr);
+        }
+  },
+
+    concatArrays: function (){
+    for(var i=0; i < hours.length; i++) {
+      //console.log('we are here' + i);
+      tokyoAnswer += `${hours[i]}: ${this.aveCookieSalesPerHr[i]} . `;
+      
+    }
+    return tokyoAnswer;
+  },
+  
+render: function(){
+  var fishData = document.getElementById('fishData');
+
+  // call an external function and get me a random number
+  // var randomNumber = random(hours.length);
+
+  // create for me a p tag and append it onto the the fishData tag
+  var thePtagForfish = document.createElement('p');
+  thePtagForfish.textContent = this.name;
+  fishData.appendChild(thePtagForfish);
+
+  // create for me another p tag and set the value to a specific item in the array
+  var output = document.createElement('p');
+  output.textContent = hours[this.getRandom];
+  fishData.appendChild(output);
+
+  // output all of the toys possible onto the DOM
+  var ulID = document.getElementById('ulID');
+  for(var i=0; i <hours.length; i++){
+    var newTag = document.createElement('li');
+    newTag.textContent = hours[i] + ': ' + this.aveCookieSalesPerHr[i];
+
+    ulID.appendChild(newTag);
+  }
+
+}
+}
+
 seattle.getHourlySales();
 seattle.concatArrays();
+
+tokyo.getHourlySales();
+//tokyo.concatArrays();
+
+
+
+
+
+
+
+
+
+
+var dubai = {
+
+  name: 'dubai',
+  maxCustomers: 38,
+  minCustomers: 11,
+  avgCookiesPerC: 3.7, 
+  aveCookieSalesPerHr: [],
+  
+
+
+  getRandom: function() {
+    var final = (Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
+    return Math.floor(final);
+  },
+
+
+  getHourlySales: function() {
+        var totalCookiesPerHour = 0;
+
+      for (var i=0; i<hours.length; i++) {
+        var customersPerHour = this.getRandom();
+        console.log(customersPerHour)
+        var totalCookiesPerHour = Math.floor((customersPerHour * this.avgCookiesPerC));
+        this.aveCookieSalesPerHr.push(totalCookiesPerHour);
+
+        console.log(this.aveCookieSalesPerHr);
+        }
+  },
+
+    concatArrays: function (){
+    for(var i=0; i < hours.length; i++) {
+      //console.log('we are here' + i);
+      dubaiAnswer += `${hours[i]}: ${this.aveCookieSalesPerHr[i]} . `;
+      
+    }
+    return dubaiAnswer;
+  },
+  
+render: function(){
+  var fishData = document.getElementById('fishData');
+
+  // call an external function and get me a random number
+  // var randomNumber = random(hours.length);
+
+  // create for me a p tag and append it onto the the fishData tag
+  var thePtagForfish = document.createElement('p');
+  thePtagForfish.textContent = this.name;
+  fishData.appendChild(thePtagForfish);
+
+  // create for me another p tag and set the value to a specific item in the array
+  var output = document.createElement('p');
+  output.textContent = hours[this.getRandom];
+  fishData.appendChild(output);
+
+  // output all of the toys possible onto the DOM
+  var ulID = document.getElementById('ulID');
+  for(var i=0; i <hours.length; i++){
+    var newTag = document.createElement('li');
+    newTag.textContent = hours[i] + ': ' + this.aveCookieSalesPerHr[i];
+
+    ulID.appendChild(newTag);
+  }
+
+}
+}
+
+dubai.getHourlySales();
+//seattle.concatArrays();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -85,10 +252,14 @@ seattle.concatArrays();
 // DOM Manipulation
 var parent = document.getElementById('header');
 
-var potato = document.createElement('h1');
-potato.textContent = "Hello Shelby!";
-parent.appendChild(potato);
+var titleVar = document.createElement('h1');
+titleVar.textContent = "Salmon Cookies!";
+parent.appendChild(titleVar);
+
+
 
 
 
 seattle.render();
+tokyo.render();
+dubai.render();
